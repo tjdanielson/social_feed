691c68ccc7b436d01cd70bf331b620e85b2d6d96
tjdanielson <tessajdanielson@gmail.com>
2022-04-04 11:54:58 -0500

    Added component file structure to project. + Created Nav bar in NavBar.jsx and imported to App.js.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
+import NavBar from './Components/NavBar/NavBar';
 
 
 function App() {
   return (
     <div >
-      
+      <NavBar />
     </div>
   );
 }
---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const NavBar = (props) => {
+    return ( 
+        <div>
+        <h3>Social 
+        <small>Feed</small></h3>
+        </div>
+     );
+}
+ 
+export default NavBar;
