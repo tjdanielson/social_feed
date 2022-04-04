db99bcf435bee3f1584cd03159b8ff080a94fb2b
tjdanielson <tessajdanielson@gmail.com>
2022-04-04 14:02:34 -0500

    Built useState variables and handlesubmit function for CreatePost component.

--- src/Components/CreatePost/CreatePost.jsx
+++ src/Components/CreatePost/CreatePost.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const CreatePost = (props) => {
+
+    const [name, setName] = useState('');
+    const [post, setPost] = useState('');
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        let newPost = {
+            name: name,
+            post: post,
+        };
+        console.log(newPost)
+    }
+
     return ( 
-        <form>
+        <form onSubmit={handleSubmit}>
             <div>
                 <label>Name</label>
-                <input type='text'/>
+                <input type='text' value={name} onChange={(event) => setName(event.target.value)} />
             </div>
             <div>
                 <label>Post</label>
-                <input type='text'/>
+                <input type='text' value={post} onChange={(event) => setPost(event.target.value)} />
             </div>
             <button type='submit'>Submit</button>
         </form>
